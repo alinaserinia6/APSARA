2e4eb6db0217925ff68d8a98612d0bbd78a5d919
ali naser <naseriniaa@gmail.com>
2026-02-17 02:22:06 +0330

    cargo init

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use dialoguer::{Input, theme::ColorfulTheme};
+use rand::RngExt;
+
+mod switch;
+use switch::Switch;
+
+fn main() {
+    let (test_numbers, ports_number, load) = get_input();
+    let mut rng = rand::rng();
+    let mut my_switch = Switch::new(ports_number);
+
+    for _ in 0..test_numbers {
+        for input_port in 0..ports_number {
+            let random_number = rng.random_range(0..100);
+            if random_number < load {
+                let output_port = rng.random_range(0..ports_number);
+                my_switch.add_a_packet_in_queue(input_port, output_port);
+            }
+        }
+    }
+}
+
+fn get_input() -> (u32, usize, u8) {
+    let test_numbers: u32 = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("1. test numbers: ")
+        .interact_text()
+        .unwrap();
+
+    let ports_number: usize = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("2. port numbers: ")
+        .interact_text()
+        .unwrap();
+
+    let load: u8 = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("3. load: [0-100]")
+        .validate_with(|input: &u8| {
+            if (0..=100).contains(input) {
+                Ok(())
+            } else {
+                Err("load value should be between 0 and 100")
+            }
+        })
+        .interact_text()
+        .unwrap();
+
+    (test_numbers, ports_number, load)
+}
---
+++ src/switch.rs
@@ -0,0 +1,58 @@
+pub struct Switch {
+    ports_number: usize,
+    queue: Vec<Vec<u32>>,
+    matching: Vec<usize>,
+    time: i32,
+    throughput: i32,
+    packets_number: u32,
+}
+
+impl Switch {
+    pub fn new(ports_number: usize) -> Self {
+        Self {
+            ports_number: ports_number,
+            queue: vec![vec![0; ports_number]; ports_number],
+            matching: vec![0; ports_number],
+            time: 0,
+            throughput: 0,
+            packets_number: 0,
+        }
+    }
+
+    pub fn add_a_packet_in_queue(&mut self, input_port: usize, output_port: usize) {
+        self.queue[input_port][output_port] += 1;
+        self.packets_number += 1;
+    }
+
+    pub fn next_match(&mut self) {}
+
+    fn get_best_neighbor(&self) {
+        let (mut I, mut J) = (0, 0);
+        let cost = self.get_cost();
+        let mut max_cost = cost;
+
+        for i in 0..self.ports_number {
+            let cost_i = self.queue[i][self.matching[i]];
+            for j in (i + 1)..self.ports_number {
+                let cost_j = self.queue[j][self.matching[j]];
+                let final_cost = cost - cost_i - cost_j
+                    + self.queue[i][self.matching[j]]
+                    + self.queue[j][self.matching[i]];
+                if final_cost > max_cost {
+                    max_cost = final_cost;
+                    (I, J) = (i, j);
+                }
+            }
+        }
+
+
+    }
+
+    fn get_cost(&self) -> u32 {
+        let mut res = 0;
+        for i in 0..self.ports_number {
+            res += self.queue[i][self.matching[i]];
+        }
+        res
+    }
+}

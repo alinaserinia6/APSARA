a9276c5453c4d0d4abbec5daa17de105521e4a7e
ali naser <naseriniaa@gmail.com>
2026-02-20 00:12:21 +0330

    complete code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 use dialoguer::{Input, theme::ColorfulTheme};
 use rand::RngExt;
 
-mod hamiltonian;
 mod switch;
 use switch::Switch;
 
 fn main() {
-    let (test_numbers, ports_number, load) = get_input();
+    // let (test_numbers, ports_number, load) = get_input();
+    let test_numbers: u32 = 131053;
+    let load: u8 = 80;
+
+    println!("test numbers: {}", test_numbers);
+    println!("load: {}", load);
+    println!("===========");
+
+    for ports_number in 4..9 {
+        println!("ports number: {}", ports_number);
+        let final_throughput = calculate_throughput(test_numbers, ports_number, load);
+        println!("final throughput: {}", final_throughput);
+        println!("####################################");
+    }
+}
+
+fn calculate_throughput(test_numbers: u32, ports_number: usize, load: u8) -> f64 {
     let mut rng = rand::rng();
     let mut my_switch = Switch::new(ports_number);
 
@@ -18,13 +33,18 @@ fn main() {
                 my_switch.add_a_packet_in_queue(input_port, output_port);
             }
         }
+        my_switch.next_match();
+        my_switch.process();
     }
+    my_switch.get_resault()
 }
 
+#[allow(dead_code)]
 /// Get User Inputs (number of tests, number of ports, load percent)
 fn get_input() -> (u32, usize, u8) {
     let test_numbers: u32 = Input::with_theme(&ColorfulTheme::default())
         .with_prompt("1. test numbers: ")
+        .default(131053)
         .interact_text()
         .unwrap();
 
@@ -42,6 +62,7 @@ fn get_input() -> (u32, usize, u8) {
                 Err("load value should be between 0 and 100")
             }
         })
+        .default(80)
         .interact_text()
         .unwrap();
 
--- src/switch.rs
+++ src/switch.rs
@@ -1,11 +1,10 @@
-use crate::hamiltonian::get_hamiltonian;
-
 pub struct Switch {
     ports_number: usize,
     queue: Vec<Vec<u32>>,
     matching: Vec<usize>,
+    hamilton_matching: Vec<usize>,
     time: u32,
-    throughput: i32,
+    throughput: u32,
     packets_number: u32,
 }
 
@@ -15,6 +14,7 @@ impl Switch {
             ports_number: ports_number,
             queue: vec![vec![0; ports_number]; ports_number],
             matching: (0..ports_number).collect(),
+            hamilton_matching: (0..ports_number).collect(),
             time: 0,
             throughput: 0,
             packets_number: 0,
@@ -26,37 +26,87 @@ impl Switch {
         self.packets_number += 1;
     }
 
-    pub fn next_match(&mut self) {
-        let (I, J, best_neighbor_cost) = self.get_best_neighbor();
-    }
-
-    fn get_best_neighbor(&self, &mut matching) -> u32 {
-        let (mut I, mut J) = (0, 0);
-        let cost = self.get_cost(&self.matching);
-        let mut max_cost = cost;
+    pub fn process(&mut self) {
+        let matching = &self.matching;
+        let queue = &mut self.queue;
 
         for i in 0..self.ports_number {
-            let cost_i = self.queue[i][self.matching[i]];
-            for j in (i + 1)..self.ports_number {
-                let cost_j = self.queue[j][self.matching[j]];
-                let final_cost = cost - cost_i - cost_j
-                    + self.queue[i][self.matching[j]]
-                    + self.queue[j][self.matching[i]];
-                if final_cost > max_cost {
-                    max_cost = final_cost;
-                    (I, J) = (i, j);
+            if queue[i][matching[i]] > 0 {
+                queue[i][matching[i]] -= 1;
+                self.throughput += 1;
+            }
+        }
+        self.time += 1;
+    }
+
+    pub fn get_resault(&self) -> f64 {
+        let throughput: f64 = self.throughput.into();
+        let packets_number: f64 = self.packets_number.into();
+
+        throughput / packets_number
+    }
+
+    pub fn next_match(&mut self) {
+        let ports = self.ports_number;
+        let queue = &self.queue;
+        let neighbor_cost = Self::get_best_neighbor_cost(queue, ports, &mut self.matching);
+        Self::get_hamiltonian(&mut self.hamilton_matching);
+        let hamilton_cost = Self::get_cost(queue, &self.hamilton_matching);
+
+        if neighbor_cost < hamilton_cost {
+            self.matching = self.hamilton_matching.clone();
+        }
+    }
+
+    fn get_best_neighbor_cost(
+        queue: &[Vec<u32>],
+        ports_number: usize,
+        matching: &mut [usize],
+    ) -> u32 {
+        let (mut ci, mut cj) = (0, 0);
+        let cost = Self::get_cost(&queue, &matching);
+        let mut max_cost = cost;
+
+        for i in 0..ports_number {
+            let cost_i = queue[i][matching[i]];
+            for j in (i + 1)..ports_number {
+                let cost_j = queue[j][matching[j]];
+                let swaped_cost =
+                    cost - cost_i - cost_j + queue[i][matching[j]] + queue[j][matching[i]];
+                if swaped_cost > max_cost {
+                    max_cost = swaped_cost;
+                    (ci, cj) = (i, j);
                 }
             }
         }
+        matching.swap(ci, cj);
 
-        (I, J, cost)
+        max_cost
     }
 
-    fn get_cost(&self, matching: &Vec<usize>) -> u32 {
-        let mut res: u32 = 0;
-        for (i, &out_port) in matching.iter().enumerate() {
-            res += self.queue[i][out_port];
+    fn get_cost(queue: &[Vec<u32>], matching: &[usize]) -> u32 {
+        matching.iter().enumerate().map(|(i, &o)| queue[i][o]).sum()
+    }
+
+    fn get_hamiltonian(matching: &mut Vec<usize>) {
+        let mut k = matching.len() - 2;
+        while matching[k] > matching[k + 1] {
+            k -= 1;
         }
-        res
+        if k == 0 {
+            return;
+        }
+        let mut j = matching.len() - 1;
+        while matching[k] > matching[j] {
+            j -= 1;
+        }
+        matching.swap(j, k);
+        let mut r = matching.len() - 1;
+        k += 1;
+        while r > k {
+            matching.swap(r, k);
+            r -= 1;
+            k += 1;
+        }
     }
 }
